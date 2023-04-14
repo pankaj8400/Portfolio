d6bf351103f6ec7225b07dcf199c84bcb25ca619
pankaj8400 <111685128+pankaj8400@users.noreply.github.com>
2023-04-14 23:22:28 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+var btn = document.getElementById("btn");
+var light = document.getElementById("light");
+function togglebtn(){
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+}
