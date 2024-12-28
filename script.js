02f5dd54123384f09556c6c0ce5ea4094a9c82d0
Pankaj Kumar Dubey <pankaj2001gkp@gmail.com>
2024-12-28 18:01:45 +0530

    responsive design

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ function togglebtn(){
     btn.classList.toggle("active");
     light.classList.toggle("on");
 }
+function toggleMenu() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.toggle('show');
+}
